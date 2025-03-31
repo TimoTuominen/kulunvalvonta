54d12735d6ea764f4bce9b2952e4974b55d5b9b5
Timo Tuominen <timo.tuominen@student.laurea.fi>
2025-03-31 21:15:52 +0300

    Tablemodels

---
+++ kulunvalvonta/kulunvalvonta/Data/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kulunvalvonta.Data.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+
+        [Required]
+        public string LocationName { get; set; }
+
+
+    }
+}
---
+++ kulunvalvonta/kulunvalvonta/Data/Models/Trafficdata.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kulunvalvonta.Data.Models
+{
+    public class Trafficdata
+    {
+
+        [Key]
+        public Ulid Id { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string RegNumber { get; set; }
+
+        public string DriverName { get; set; }
+
+        [StringLength(100)]
+        public string Company { get; set; }
+
+        [StringLength(30)]
+        public string PhoneNumber { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public TimeOnly EntryTime { get; set; }
+
+        public TimeOnly ExitTime { get; set; }
+
+        [Required]
+        public int LocationId { get; set; }
+
+    }
+}
